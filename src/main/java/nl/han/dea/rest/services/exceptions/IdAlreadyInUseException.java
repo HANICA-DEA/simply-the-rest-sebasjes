317f64566b894f7fb576bc6985c212e9e0290746
sebasjes <sebas.verhagen@hotmail.nl>
2021-03-02 16:50:16 +0100

    start opdracht 10, 404 status afgehandelt, 409 status nog niet werkend.

--- src/main/java/nl/han/dea/rest/services/exceptions/IdAlreadyInUseException.java
+++ src/main/java/nl/han/dea/rest/services/exceptions/IdAlreadyInUseException.java
@@ -1,4 +1,14 @@
 package  nl.han.dea.rest.services.exceptions;
 
-public class IdAlreadyInUseException extends RuntimeException {
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class IdAlreadyInUseException extends RuntimeException implements ExceptionMapper {
+
+    @Override
+    public Response toResponse(Throwable throwable) {
+        return Response.status(Response.Status.CONFLICT).build();
+    }
 }
--- src/main/java/nl/han/dea/rest/services/exceptions/ItemNotAvailableException.java
+++ src/main/java/nl/han/dea/rest/services/exceptions/ItemNotAvailableException.java
@@ -1,4 +1,14 @@
 package  nl.han.dea.rest.services.exceptions;
 
-public class ItemNotAvailableException extends RuntimeException {
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class ItemNotAvailableException extends RuntimeException implements ExceptionMapper {
+
+    @Override
+    public Response toResponse(Throwable throwable) {
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
 }
